68cb6bfd94a52b0fc78ee0a4b4014a5ed515f235
moqada <moqada@gmail.com>
2016-03-26 05:24:22 +0900

    mas: Add mas-install.sh
    - execute mas install

---
+++ scripts/mas-install.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+mas install 568494494  # Pocket
+mas install 539883307  # LINE
+mas install 414855915  # WinArchiver Lite
+mas install 409183694  # Keynote
+mas install 427515976  # 3Hub
+mas install 408981381  # iPhoto
+mas install 682658836  # GarageBand
+mas install 467939042  # Growl
+mas install 557168941  # Tweetbot
+mas install 992076693  # MindNode
+mas install 497799835  # Xcode
+mas install 409203825  # Numbers
+mas install 409201541  # Pages
+mas install 408981434  # iMovie
+mas install 918858936  # Airmail 2
+mas install 415139197  # WhiteNoise
+mas install 886106985  # Sunrise Calendar
+mas install 715768417  # MicrosoftRemoteDesktop
+mas install 1055511498 # Day One
+mas install 449589707  # Dash
+mas install 402398561  # MindNode Pro
