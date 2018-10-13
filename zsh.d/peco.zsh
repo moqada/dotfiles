17d858ee2bcec691a84747950d80291d0afbd12d
moqada <moqada@gmail.com>
2018-10-13 19:07:54 +0900

    git,zsh: Update settings for searching repository via peco
    - source directory をひとつにして full path を表示するのをやめた

--- zsh.d/peco.zsh
+++ zsh.d/peco.zsh
@@ -27,9 +27,9 @@ alias pk="peco-pkill"
 
 # ghq cd
 function peco-src() {
-    local selected_dir=$(ghq list -p | peco --query "$LBUFFER")
+    local selected_dir=$(ghq list | peco --query "$LBUFFER")
     if [ -n "$selected_dir" ]; then
-        BUFFER="cd ${selected_dir}"
+        BUFFER="cd $(ghq root)/${selected_dir}"
         zle accept-line
     fi
     zle redisplay
